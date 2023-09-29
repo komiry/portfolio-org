66875330f9404385f0a0784456a24fb4d8cd062f
komiry <ryagondev@gmail.com>
2023-09-30 05:08:56 +0800

    last

--- script.js
+++ script.js
@@ -5,24 +5,6 @@ const pageTitle = document.getElementById("page-title");
 const lightMode = document.getElementById("sun");
 const body = document.querySelector("body");
 
-// let typed = new Typed("#myname", {
-//   strings: ["LEMUEL INCISO"],
-//   typeSpeed: 70,
-//   backSpeed: 110,
-//   loop: false,
-
-//   showCursor: false,
-//   startDelay: 100,
-// });
-
-// new TypeIt("#myname", {
-//   strings: "NICE TO MEET YOU!",
-//   speed: 75,
-//   afterComplete: function (instance) {
-//     instance.destroy();
-//   },
-// }).go();
-
 new TypeIt("#myname", {
   speed: 90,
   afterComplete: function (instance) {
@@ -33,12 +15,9 @@ new TypeIt("#myname", {
   .pause(1000)
   .delete(17)
   .pause(1000)
-  .type("LENUEL", { delay: 300 })
-  .move(-3)
-  .delete(1)
-  .type("M")
+  .type("Ryan ", { delay: 300 })
   .move(null, { to: "END" })
-  .type(" INCISO")
+  .type("Dela Cruz")
   .go();
 
 new TypeIt("#hey", {
@@ -71,7 +50,7 @@ function showPage(index) {
     page.style.display = pageIndex === index ? "flex" : "none";
   });
 
-  const pageTitleText = pages[index].classList[0]; // Assumes the second class is the page title
+  const pageTitleText = pages[index].classList[0];
   pageTitle.textContent = pageTitleText;
 }
 
